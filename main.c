aa567a4a7bd7a2b02e5e9f957413225e04c3336b
Katyanna Moura <amelie.kn@gmail.com>
2015-06-14 17:15:40 -0300

    Populates universe

--- main.c
+++ main.c
@@ -4,6 +4,22 @@
 
 #define for_column_by_line for_column for_line
 
+void show(void *u, int w, int h){
+  int (*universe)[w] = u;
+  printf("\033[H");
+  for (int line = 0; line < h; line++) {
+    for (int column = 0; column < w; column++) {
+    	if (universe[line][column]) {
+    		printf("#");
+    	} else {
+    		printf("  ");
+    	}
+    }
+    printf("\n");
+  }
+  fflush(stdout);
+}
+
 void game(int w, int h){
   unsigned universe[h][w];
   for_column_by_line universe[line][column] = return rand() < RAND_MAX / 5 ? 1 : 0;
